46e86e7a868d0388e68ef471e1428f690eb72e9d
Samir Machkour <s.machkour01@gmail.com>
2024-03-27 13:42:43 +0100

    lab3 + lab4vraag3

--- LAB2/InterruptSignalApp_1_system/_ide/scripts/debugger_interruptsignalapp_1-default.tcl
+++ LAB2/InterruptSignalApp_1_system/_ide/scripts/debugger_interruptsignalapp_1-default.tcl
@@ -2,28 +2,28 @@
 # In Vitis IDE create a Single Application Debug launch configuration,
 # change the debug type to 'Attach to running target' and provide this 
 # tcl script in 'Execute Script' option.
-# Path of this script: D:\devWorks\PBEAICT\J2\S2\AP\LAB2\InterruptSignalApp_1_system\_ide\scripts\debugger_interruptsignalapp_1-default.tcl
+# Path of this script: C:\Users\machk\gitkraken\Audio_Processing\LAB2\InterruptSignalApp_1_system\_ide\scripts\debugger_interruptsignalapp_1-default.tcl
 # 
 # 
 # Usage with xsct:
 # To debug using xsct, launch xsct and run below command
-# source D:\devWorks\PBEAICT\J2\S2\AP\LAB2\InterruptSignalApp_1_system\_ide\scripts\debugger_interruptsignalapp_1-default.tcl
+# source C:\Users\machk\gitkraken\Audio_Processing\LAB2\InterruptSignalApp_1_system\_ide\scripts\debugger_interruptsignalapp_1-default.tcl
 # 
 connect -url tcp:127.0.0.1:3121
 targets -set -nocase -filter {name =~"APU*"}
 rst -system
 after 3000
 targets -set -filter {jtag_cable_name =~ "Xilinx TUL 1234-tulA" && level==0 && jtag_device_ctx=="jsn-TUL-1234-tulA-23727093-0"}
-fpga -file D:/devWorks/PBEAICT/J2/S2/AP/LAB2/InterruptSignalApp_1/_ide/bitstream/AudioProcessing2.bit
+fpga -file C:/Users/machk/gitkraken/Audio_Processing/LAB2/InterruptSignalApp_1/_ide/bitstream/AudioProcessing2.bit
 targets -set -nocase -filter {name =~"APU*"}
 loadhw -hw D:/devWorks/PBEAICT/J2/S2/AP/LAB2/Lab2_platform/export/Lab2_platform/hw/AudioProcessing2.xsa -mem-ranges [list {0x40000000 0xbfffffff}] -regs
 configparams force-mem-access 1
 targets -set -nocase -filter {name =~"APU*"}
-source D:/devWorks/PBEAICT/J2/S2/AP/LAB2/InterruptSignalApp_1/_ide/psinit/ps7_init.tcl
+source C:/Users/machk/gitkraken/Audio_Processing/LAB2/InterruptSignalApp_1/_ide/psinit/ps7_init.tcl
 ps7_init
 ps7_post_config
 targets -set -nocase -filter {name =~ "*A9*#0"}
-dow D:/devWorks/PBEAICT/J2/S2/AP/LAB2/InterruptSignalApp_1/Debug/InterruptSignalApp_1.elf
+dow C:/Users/machk/gitkraken/Audio_Processing/LAB2/InterruptSignalApp_1/Debug/InterruptSignalApp_1.elf
 configparams force-mem-access 0
 targets -set -nocase -filter {name =~ "*A9*#0"}
 con
---
+++ LAB4/Vraag3_App_system/_ide/scripts/debugger_vraag3_app-default.tcl
@@ -0,0 +1,29 @@
+# Usage with Vitis IDE:
+# In Vitis IDE create a Single Application Debug launch configuration,
+# change the debug type to 'Attach to running target' and provide this 
+# tcl script in 'Execute Script' option.
+# Path of this script: C:\Users\machk\gitkraken\Audio_Processing\LAB4\Vraag3_App_system\_ide\scripts\debugger_vraag3_app-default.tcl
+# 
+# 
+# Usage with xsct:
+# To debug using xsct, launch xsct and run below command
+# source C:\Users\machk\gitkraken\Audio_Processing\LAB4\Vraag3_App_system\_ide\scripts\debugger_vraag3_app-default.tcl
+# 
+connect -url tcp:127.0.0.1:3121
+targets -set -nocase -filter {name =~"APU*"}
+rst -system
+after 3000
+targets -set -filter {jtag_cable_name =~ "Xilinx TUL 1234-tulA" && level==0 && jtag_device_ctx=="jsn-TUL-1234-tulA-23727093-0"}
+fpga -file C:/Users/machk/gitkraken/Audio_Processing/LAB4/Vraag3_App/_ide/bitstream/AudioProcessing2.bit
+targets -set -nocase -filter {name =~"APU*"}
+loadhw -hw C:/Users/machk/gitkraken/Audio_Processing/LAB4/AudioProcessing2/export/AudioProcessing2/hw/AudioProcessing2.xsa -mem-ranges [list {0x40000000 0xbfffffff}] -regs
+configparams force-mem-access 1
+targets -set -nocase -filter {name =~"APU*"}
+source C:/Users/machk/gitkraken/Audio_Processing/LAB4/Vraag3_App/_ide/psinit/ps7_init.tcl
+ps7_init
+ps7_post_config
+targets -set -nocase -filter {name =~ "*A9*#0"}
+dow C:/Users/machk/gitkraken/Audio_Processing/LAB4/Vraag3_App/Debug/Vraag3_App.elf
+configparams force-mem-access 0
+targets -set -nocase -filter {name =~ "*A9*#0"}
+con
